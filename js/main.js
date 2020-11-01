c51f298d68ad39ea169d6cb0a2a70d009edc7022
viper12590 <viper1259@gmail.com>
2020-11-01 09:52:57 +0000

    attribute and css style change

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 function helloWorld() {
     document.getElementById("helloWorld").innerHTML = "Hello world!";
 }
+
+function biggerImage() {
+    document.getElementById("smallImage").src = "https://via.placeholder.com/300x300";
+    document.getElementById("smallImage").width = "300";
+    document.getElementById("smallImage").height = "300";
+}
+
+function textColourChange() {
+    var r = Math.floor(Math.random() * 256).toString(16);
+    var g = Math.floor(Math.random() * 256).toString(16);
+    var b = Math.floor(Math.random() * 256).toString(16);
+    document.getElementById("targetText").style.color = "#"+r+g+b;
+}
